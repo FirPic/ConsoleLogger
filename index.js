6022a1865f3db2089c39ab1b98929ae9c140c29e
ConsoleLogger <tiago.792918@gmail.com>
2022-03-17 10:51:02 +0100

    v1.0.3

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ const fs = require('fs');
     function Log(str, lvl){
         const date = new Date();
         const dateLog = `${date.getDay()}/${date.getMonth()}/${date.getFullYear()}|${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
-        const log = `[${dateLog}][${lvl.toUpperCase()}]: ${str} \n`;
+        const log = `[${dateLog}][${lvl/*.toUpperCase()*/}]: ${str}`;
         /*if(!path) console.log(`Path Error, the path is emplty`);
         else*/ 
         if(!str) console.log(`Log Error, the content is empty `);
         else if(!lvl) console.log(`Log Error, the level is empty`);
-            else{ File(log); console.log(log);}
+            else{ File(`${log}\n`); console.log(log);}
         
     }
 exports.Log=Log;
