ac5269a19632611a0780debbcfadca51d33dc038
ConsoleLogger <tiago.792918@gmail.com>
2022-03-17 21:44:59 +0100

    v1.1.0

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
 const fs = require('fs');
 
-    /*const path='';
-    exports.Path=path;*/
+    let path='./debug.log';
+    exports.Path=path;
+
+    const date = new Date();
+    const dateLog = `${date.getDay()}/${date.getMonth()}/${date.getFullYear()}|${date.getHours()}:${date.getMinutes()}:${getSeconds()}`;
+
+    function getSeconds(){
+        if(date.getSeconds()<=9){
+            return `0${date.getSeconds()}`;
+        }else{
+            return date.getSeconds();
+        }
+    }
+
+
 
     function File(content){
-        fs.writeFile('./debug.log', content, {flag:'a+'}, err=>{
-            if(err) console.log(`File Error, the file not created : ${err}`);
+        fs.writeFile(path, content, {flag:'a+'}, err=>{
+            if(err) console.log(`[${dateLog}][ERROR]: File Error, the file not created : ${err}`);
         })
     }
     
     function Log(str, lvl){
-        const date = new Date();
-        const dateLog = `${date.getDay()}/${date.getMonth()}/${date.getFullYear()}|${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
         const log = `[${dateLog}][${lvl/*.toUpperCase()*/}]: ${str}`;
         /*if(!path) console.log(`Path Error, the path is emplty`);
         else*/ 
-        if(!str) console.log(`Log Error, the content is empty `);
-        else if(!lvl) console.log(`Log Error, the level is empty`);
+        if(!str) console.log(`[${dateLog}][ERROR]: Log Error, the content is empty `);
+        else if(!lvl) console.log(`[${dateLog}][ERROR]: Log Error, the level is empty`);
             else{ File(`${log}\n`); console.log(log);}
         
     }
